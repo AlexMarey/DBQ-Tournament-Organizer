b00fcaa2a648c19f66027d014504e5f7f2c31969
AlexMarey <alx.9.mar@gmail.com>
2020-04-26 22:28:53 -0400

    removed args portion of help command

--- src/commands/Help.js
+++ src/commands/Help.js
@@ -1,30 +1,33 @@
 module.exports = {
-    name: "help",
-    description: "List all available commands or gives specific help for one command",
-    args: false,
-    usage: "",
-    requiresAdmin: false,
-    execute(message, args={}) {
-        var reply = "";
-        if (args.length >= 1) {
-            message.channel.send(`Help command with at least 1 arg`);
-        } else {            
-            reply += "```Commands:\n"
-            args.forEach(command => {
-                if(command.requiresAdmin && message.member.hasPermission('ADMINISTRATOR')){
-                    reply += formatReply(command);
-                } else {
-                    reply += formatReply(command);
-            }});
-            reply += "```"
-        }
-        message.channel.send(reply);
-    }
+  name: "help",
+  description:
+    "List all available commands or gives specific help for one command",
+  args: false,
+  usage: "",
+  requiresAdmin: false,
+  execute(message, args = {}) {
+    var reply = "";
+    reply += "```Commands:\n";
+    
+    args.forEach((command) => {
+      if (
+        command.requiresAdmin &&
+        message.member.hasPermission("ADMINISTRATOR")
+      ) {
+        reply += formatReply(command);
+      } else {
+        reply += formatReply(command);
+      }
+    });
+    reply += "```";
+
+    message.channel.send(reply);
+  },
 };
 
 function formatReply(command) {
-    let reply = `  - ${command.name}`;
-    reply += command.usage !='' ? ` ${command.usage}` : ''; 
-    reply += `: ${command.description} \n`;
-    return reply;
+  let reply = `  - ${command.name}`;
+  reply += command.usage != "" ? ` ${command.usage}` : "";
+  reply += `: ${command.description} \n`;
+  return reply;
 }
