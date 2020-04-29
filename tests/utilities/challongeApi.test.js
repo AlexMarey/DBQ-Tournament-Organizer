b27e1728676dcdc53c5942abee5e6f0d5757a742
Alex Marey <armv26@mst.edu>
2020-04-29 19:07:27 -0400

    Testing that a participant has been provided to the add participant utility

--- tests/utilities/challongeApi.test.js
+++ tests/utilities/challongeApi.test.js
@@ -1,4 +1,5 @@
 const { addParticipantToTournament } = require('../../src/utilities/challongeApi');
+const postData = require('../../src/utilities/postData');
 
 describe('ChallongeApi', () => {
     test('Should return an error message when the url is empty', () => {
@@ -11,5 +12,15 @@ describe('ChallongeApi', () => {
         var reply = addParticipantToTournament(url, particpant);
 
         expect(reply).toBe(expectedReply);
+    });
+
+    test('Should return an error message when the participants is empty', () => {
+        var particpant = {};
+        var url = 'url.com';
+        var expectedReply = `I didn't catch who wanted to sign up. Try again or use a different command.`;
+
+        var reply = addParticipantToTournament(url, particpant);
+
+        expect(reply).toBe(expectedReply);    
     });
 });
